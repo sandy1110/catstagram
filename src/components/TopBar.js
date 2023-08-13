79780a0e732dda649a496f1151e9057670a6eec9
sandy1110 <sandiig.1110@gmail.com>
2023-08-12 23:35:14 -0600

    tailwind addition

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,21 +1,31 @@
-import React from "react";
 import "../styles/styles.css"
+import React, { useState } from "react";
+import { Dropdown } from 'primereact/dropdown';
 import catLogo from "../images/catLogo.png"
 
 const TopBar = () => {
+    const [selectedBreed, setSelectedBreed] = useState(null);
+    const cities = [
+        { name: 'New York', code: 'NY' },
+        { name: 'Rome', code: 'RM' },
+        { name: 'London', code: 'LDN' },
+        { name: 'Istanbul', code: 'IST' },
+        { name: 'Paris', code: 'PRS' }
+    ];
     return (
+        <>
         <div className="app-bar">
             <div className="logo-name">
                 <img className="cat-logo" src={catLogo} alt="Logo de catstagram" />
                 <h1>Catstagram</h1>
             </div> 
-            <div className="breed-selector">
-                <select className="breed-selector">
-                    <option>Breed</option>
-
+            <div className="card flex justify-content-center">
+                <select>
+                    <option>opcion</option>
                 </select>
+
             </div> 
-        </div>
+        </div></>
     );
 }
 
