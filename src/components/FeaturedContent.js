087cbb6ff24434650aac899313993acd641a3b50
sandy1110 <sandiig.1110@gmail.com>
2023-08-14 03:14:38 -0600

    featured cats still not working

--- src/components/FeaturedContent.js
+++ src/components/FeaturedContent.js
@@ -5,40 +5,36 @@ import FeaturedBreed from "./FeaturedBreed";
 const FeaturedContent = (props) => {
     const breeds = props.breeds;
     const [randomBreeds, setRandomBreeds] = useState([]);
-
     const [catsArray, setCatsArray] = useState([]);
 
     useEffect(()=>{
-
         let lista=[];
         for(let i=0;i<=66;i++){
             lista.push(i);
         }
         lista = lista.sort(function() {return Math.random() - 0.5});
         setRandomBreeds(lista.splice(0,7));
-
-        randomBreeds.map(cat =>{
-            const catId = breeds[cat].id;
-            console.log(catId);
-            fetch(`https://api.thecatapi.com/v1/images/search?breed_ids=${catId}`)
-            .then(res=>{ return res.json()})
-            .then(res=>{
-                console.log("image",res[0].url);
-                setCatsArray(catsArray.push(res[0].url));
-                console.log(catsArray);
-            })
-        })
     },[]);
+
+    const getFeaturedPicture = (cat) =>{
+        const catId = breeds[cat].id;
+        console.log(catId);
+        fetch(`https://api.thecatapi.com/v1/images/search?breed_ids=${catId}`)
+        .then(res=>{ return res.json()})
+        .then(res=>{
+            const catPicture = res[0].url;
+            console.log("image",catPicture);
+            setCatsArray([...catsArray, catPicture]);
+        })
+    }
 
         
     return (
         <>
         <div className="featured-breeds">
-            {breeds === null ? <h2>Loading...</h2> :randomBreeds.map(breedId =>
-                
-            (
-                <FeaturedBreed key={breedId} image={"https://cdn2.thecatapi.com/images/24p.jpg"} name={breeds[breedId].name}/>
-            ))}
+            {breeds === null ? <h2>Loading...</h2> : randomBreeds.map(breedId =>
+                (<FeaturedBreed key={breedId} image={"https://cdn2.thecatapi.com/images/24p.jpg"} name={breeds[breedId].name}/>
+                ))}
         </div></>
     );
 }
